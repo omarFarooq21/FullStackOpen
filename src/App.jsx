1bf69902ca83a31cc4f544228019b762b9451321
omarFarooq21 <omarf4c@gmail.com>
2024-02-11 16:57:49 +0500

    Added code from part1 tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+const Hello = (props) => {
+  if(props.age)
+  {
+    return(
+      <> 
+        <p>Hello {props.name}, you are {props.age} years old</p>
+      </>
+    )
+  }
+  
+}
 
+const App = () => {
+
+  const name = "Peter"
+  const age = 10
+  return(
+    //fragments <> used to group together multiple components and avoid "Extra" dom elements </>
+    <div>
+      <h1>
+        Greetings!
+      </h1>
+      <Hello name="Alisha"/>
+      <Hello name = {name} age={age}/>
+    </div>
+  )
+}
 export default App
-
-
