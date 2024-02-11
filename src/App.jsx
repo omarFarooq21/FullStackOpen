645142d234d7036bbcb04e95b981a762ee1029fd
omarFarooq21 <omarf4c@gmail.com>
2024-02-11 19:14:12 +0500

    1.3: course information step3

--- src/App.jsx
+++ src/App.jsx
@@ -5,54 +5,49 @@ const Header = (props) => {
     </h1>
   )
 }
-
 const Part = (props) => {
   return(
     <p>
       {props.part} - {props.exercise}
     </p>
   )
-
 }
 const Content = (props) => {
+  console.log("Content Props: ",props)
   return (
     <div>
-      <Part part = {props.content[0]} exercise = {props.content[1]} />
-      <Part part = {props.content[2]} exercise = {props.content[3]} />
-      <Part part = {props.content[4]} exercise = {props.content[5]} />
+      <Part part = {props.part1.name} exercise = {props.part1.exercises} />
+      <Part part = {props.part2.name} exercise = {props.part2.exercises} />
+      <Part part = {props.part3.name} exercise = {props.part3.exercises} />
     </div>
-     
   )
 }
-
 const Total = (props) => {
   return(
     <p>Number of Exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
-
-
 const App = () => {
- const course = "Half Stack App Development"
- const part1 = 'Fundamentals of React'
- const exercises1 = 10
- const part2 = 'Using props to pass data'
- const exercises2 = 7
- const part3 = 'State of a component'
- const exercises3 = 14
-
- const content = [ 
-  part1, exercises1, part2, exercises2, part3, exercises3
- ]
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
-     <Header course = {course}/>
-     <Content content = {content}/>
-     <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    <Header course = {course}/>
+    <Content part1 = {part1} part2 = {part2} part3 = {part3}/>
+    <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </>
-
   )
 }
-
 export default App
