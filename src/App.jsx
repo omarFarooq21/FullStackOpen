2380d24718b387e052f8f7c671980708dc320762
omarFarooq21 <omarf4c@gmail.com>
2024-02-11 17:56:19 +0500

    1.1 course infromation, step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
-const Hello = (props) => {
-  if(props.age)
-  {
-    return(
-      <> 
-        <p>Hello {props.name}, you are {props.age} years old</p>
-      </>
-    )
-  }
-  
-}
-
-const App = () => {
-
-  const name = "Peter"
-  const age = 10
-  return(
-    //fragments <> used to group together multiple components and avoid "Extra" dom elements </>
-    <div>
-      <h1>
-        Greetings!
-      </h1>
-      <Hello name="Alisha"/>
-      <Hello name = {name} age={age}/>
-    </div>
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
   )
 }
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.content.forEach( (element, index) => {
+        if(index%2 == 0)
+        {
+          const para = document.createElement('p')
+          para.textContent =  `${element} ${props.content[index+1]}`;
+          document.body.appendChild(para)
+          console.log(element, index)
+        }
+      })} 
+    </p>
+     
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of Exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+
+const App = () => {
+ const course = "Half Stack App Development"
+ const part1 = 'Fundamentals of React'
+ const exercises1 = 10
+ const part2 = 'Using props to pass data'
+ const exercises2 = 7
+ const part3 = 'State of a component'
+ const exercises3 = 14
+
+ const content = [ 
+  part1, exercises1, part2, exercises2, part3, exercises3
+ ]
+
+  return (
+    <>
+     <Header course = {course}/>
+     <Content content = {content}/>
+     <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </>
+
+  )
+}
+
 export default App
