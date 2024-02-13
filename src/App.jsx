110447ebb9614941df692be20ec5cd4952cdf4ea
omarFarooq21 <omarf4c@gmail.com>
2024-02-14 01:54:24 +0500

    Passing event handlers to components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import {useState} from 'react'
 
+const Display = (props) => {
+  return(
+    <div>
+      {props.counter}
+    </div>
+  )
+}
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
-  const handleClick = () => {
-    setCounter(counter+1)
-  }
-  // setTimeout(
-  //   () => setCounter(counter+1), 1000
-  // )
-  console.log("rendering...", counter)
+
+  const increment = () => setCounter(counter+1)
+  const reset = () => setCounter(0)
+
   return(
     <>
-    <div>
-      {counter}
-    </div>
-    <button onClick={handleClick}>
-      Increment
-    </button>
-    <button onClick={ () => setCounter(0)}>
-      Reset
-    </button>
+      <div>
+        <Display counter={counter}/>
+        <Button onClick={increment} text="Increment"/>
+        <Button onClick={reset} text="Reset"/>
+
+      </div>
     </>
     
   )
