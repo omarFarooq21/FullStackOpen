4e1886c1f035ac47997a4aa8013d512c124d7328
omarFarooq21 <omarf4c@gmail.com>
2024-02-15 23:49:31 +0500

    1.6: unicafe step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-const Display = (props) => {
-  return(
-    <div>
-      {props.counter}
-    </div>
+const Button = (props) => {
+  return( 
+    <button onClick = {props.handleClick}>
+      {props.text}
+    </button>
   )
 }
-const Button = (props) => {
+const Statistics = ({good, bad, neutral}) => {
   return(
-    <button onClick={props.onClick}>{props.text}</button>
+    <>
+     <h1>
+      Statistics
+    
+     </h1>
+     <p>good {good}</p>
+     <p>bad {bad}</p>
+     <p>neutral {neutral}</p>
+    </> 
   )
 }
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  const increment = () => setCounter(counter+1)
-  const reset = () => setCounter(0)
-
-  return(
-    <>
-      <div>
-        <Display counter={counter}/>
-        <Button onClick={increment} text="Increment"/>
-        <Button onClick={reset} text="Reset"/>
-
-      </div>
-    </>
-    
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handleGoodClick = () => {
+    setGood(good+1)
+    console.log("Good: ", good)
+  }
+  const handleBadClick = () => {
+    setBad(bad+1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1)
+  }
+  return (
+    <div>
+      <h1> Give Feedback </h1>
+      <Button text = {`Good: ${good}`} handleClick = {handleGoodClick}/>
+      <Button text = {`Bad: ${bad}`} handleClick = {handleBadClick}/>
+      <Button text = {`Neutral: ${neutral}`} handleClick= {handleNeutralClick} />
+      <Statistics good = {good} bad = {bad} neutral = {neutral}/>
+    </div>
   )
 }
+
 export default App
