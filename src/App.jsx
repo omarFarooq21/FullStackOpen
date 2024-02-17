2dbabad0eee245d3c460bf06727b902577515dec
omarFarooq21 <omarf4c@gmail.com>
2024-02-17 15:57:50 +0500

    1.11*: unicafe step6

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,70 @@ const Button = (props) => {
     </button>
   )
 }
-const Statistics = ({good, bad, neutral, allRatings}) => {
+const StatisticLine = ({text, value}) => {
+  return(
+    <>
+    <table>
+      <colgroup>
+        <col style={{width:90}}/>
+        <col/>
+      </colgroup>
+      <tbody>
+        <tr>
+          <td>
+            {text}
+          </td>
+          <td>
+            {value}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </>
+  )
+}
+const Statistics = ({good, bad, neutral, allRatings, Feedback}) => {
   let sum = 0;
   allRatings.forEach(element => {
     sum += element;
   })
   const percentGood = Math.round((good / allRatings.length)*100)
   const average = sum/allRatings.length
-  return(
-    <>
-     <h1>
-      Statistics
-    
-     </h1>
-     <p>good {good}</p>
-     <p>bad {bad}</p>
-     <p>neutral {neutral}</p>
-     <p>all {allRatings.length}</p>
-     <p>average: {average}</p>
-     <p>positive: {`${percentGood}%`}</p>
-    </> 
-  )
+  if(Feedback)
+  {
+    return(
+      <>
+       <h1>
+        Statistics
+       </h1>
+       <StatisticLine text="good" value={good}/>
+       <StatisticLine text="bad" value={bad}/>
+       <StatisticLine text="neutral" value={neutral}/>
+       <StatisticLine text="all" value={allRatings.length}/>
+       <StatisticLine text="average" value={average}/>
+       <StatisticLine text="postivie" value={`${percentGood}%`}/>
+       {/* <p>good {good}</p>
+       <p>bad {bad}</p>
+       <p>neutral {neutral}</p>
+       <p>all {allRatings.length}</p>
+       <p>average: {average}</p>
+       <p>positive: {`${percentGood}%`}</p> */}
+      </> 
+    )
+  }
+  else {
+    return(
+      <>
+        <h1>
+          Statistics
+        </h1>
+        <p>
+          No feedback given
+        </p>
+      </>
+    )
+  }
+
 }
 
 const App = () => {
@@ -36,19 +79,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [allRatings, setAll] = useState([])
-
+  const [feedback, setFeedback] = useState(false)
   const handleGoodClick = () => {
     setGood(good+1)
     console.log("Good: ", good)
     setAll(allRatings.concat(1))
+    setFeedback(true)
   }
   const handleBadClick = () => {
     setBad(bad+1)
     setAll(allRatings.concat(-1))
+    setFeedback(true)
   }
   const handleNeutralClick = () => {
+    console.log(feedback, "Feedback before making any changes")
+    setFeedback(true)
+    console.log(feedback, "on neutral before changing neutral")
     setNeutral(neutral+1)
     setAll(allRatings.concat(0))
+    console.log(feedback, "on neutral after setting feedback: true and neutral++")
   }
   return (
     <div>
@@ -56,7 +105,7 @@ const App = () => {
       <Button text = {`Good: ${good}`} handleClick = {handleGoodClick}/>
       <Button text = {`Bad: ${bad}`} handleClick = {handleBadClick}/>
       <Button text = {`Neutral: ${neutral}`} handleClick= {handleNeutralClick} />
-      <Statistics good = {good} bad = {bad} neutral = {neutral} allRatings = {allRatings}/>
+      <Statistics good = {good} bad = {bad} neutral = {neutral} allRatings = {allRatings} Feedback = {feedback}/>
     </div>
   )
 }
