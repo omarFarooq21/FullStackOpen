266515d9d2c223dd5debbbf215b5dce28e996f55
omarFarooq21 <omarf4c@gmail.com>
2024-02-25 21:40:58 +0500

    Part 2a

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,24 @@
-import { useState } from 'react'
+import Note from './components/Note'
+const App = ({notes}) => {
 
-const MostRatedAnecdote = (props) => {
-  const max = Math.max(...props.votes); // get the max number of votes
-  let indexOfMax // index of max votes
-  props.votes.forEach((element, index) => {
-    if(element === max) {
-      indexOfMax = index
-    }
-  });
-  console.log("index of max: ", indexOfMax)
-
-  
   return (
-    <>
-      <div>
-        {props.anecdotes[indexOfMax]} has {max} votes
-      </div>
-    </>
-  )
-}
-const Button = (props) => {
-  return(
-    <>
-      <button onClick={props.handleClick}>{props.text}</button>
-    </>
-  )
-}
-
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-
-  
-  const nextAnecdote = () => {
-    let i = Math.floor(Math.random() * anecdotes.length)
-    setSelected(i)
-  }
-  const vote = () => {
-    const newVotes = [...votes]
-    newVotes[selected]++
-    setVotes(newVotes)
-  }
-  return (
-    <>
+    <div>
       <h1>
-        Anecdote of the day
+        Notes
       </h1>
-      <div>
-        {anecdotes[selected]} has {votes[selected]} votes
-      </div>
-      <div>
-        <Button handleClick = {vote} text = "vote"/>
-        <Button handleClick = {nextAnecdote} text = "nextanecdote"/>
-      </div>
-      <div>
-        <h1>
-          Anecdotes with most votes
-        </h1>
-      </div>
-      <div>
-        <MostRatedAnecdote votes = {votes} anecdotes = {anecdotes} selected = {selected}/>
-      </div>
-    </>
-   
+      <ul>
+         {notes.map(note => <Note key = {note.id}  note={note}/>)}
+        {/* <li>
+          {notes[0].content}
+        </li>
+        <li>
+          {notes[1].content}
+        </li>
+        <li>
+          {notes[2].content}
+        </li> */}
+      </ul>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,26 @@ import ReactDOM from 'react-dom/client'
 // import './index.css'
 import App from './App'
 
+const notes = [
+  {
+    id:1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id:2,
+    content: 'Browser can only run JS',
+    important: false
+  },
+  {
+    id:3,
+    content: 'GET and POST are most important',
+    important: true
+  }
+]
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <App />
+    <App notes = {notes} />
   // </React.StrictMode>,
 )
