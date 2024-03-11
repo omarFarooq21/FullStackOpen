9943208c0f0f67b4138d0c751c4a17c04b0d72c6
omarFarooq21 <omarf4c@gmail.com>
2024-03-12 04:27:53 +0500

    2.9*: The Phonebook Step 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState( [ 
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123456789', id: 1},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
    ] )
-   const [newName, setNewName] = useState('a new name...')
+   const [newName, setNewName] = useState('')
+   const [newNumber, setNewNumber] = useState('')
+
+   //filter 2.9*
+   const [filter, setFilter] = useState('')
+   const [showAll, setShowAll] = useState(true)
+   const peopleToShow = showAll ? persons : persons.filter(person => person.name.toLowerCase().startsWith(filter))
+
 
    const addPerson = (event) => {
     event.preventDefault()
@@ -12,13 +22,15 @@ const App = () => {
 
     let added = false;
     persons.forEach((element) => {
-      if(element.name === newName)
+      if(element.name === newName && element.number === newNumber)
       {
         added = true
       }
     })
     const nameObj = {
+      id: persons.length+1,
       name: newName,
+      number: newNumber,
     }
     if(!added)
     {
@@ -26,21 +38,33 @@ const App = () => {
       alert(`added ${newName}`)
     }
     else if(added)
-    {
+    { 
       alert(`${newName} is already added!`)
     }
-    
    }
-   const handleChange = (event) => {
+   const handleNameChange = (event) => {
     setNewName(event.target.value)
+   }
+   const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+   }
+   const filterPeople = (event) => {
+    setShowAll(false)
+    setFilter(event.target.value.toLowerCase())
    }
 
    return ( 
     <div>
       <h2>Phonebook</h2>
+      <input onChange = {filterPeople}/>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type = "submit">add</button>
@@ -48,7 +72,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {/* map through the array of objects and create a list item for each object */} 
-        {persons.map((person, index) => { return(<p key = {person.name}>{person.name}</p>)})}
+        {peopleToShow.map((person) => { return(<p key = {person.id}>{person.name} {person.number}</p>)})}
     </div>
    )
 }
